56fa0cb57e4e83db928c24e841d170ef4e38d3b9
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-07-31 15:41:14 +0900

    added an array of animation image positions

--- script.js
+++ script.js
@@ -11,14 +11,37 @@ let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: "idle",
+        frames: 7,
+    }, 
+    {
+        name: "jump",
+        frames: 7,
+    }
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j=0; j<state.frames; j++) {
+        let positionX = j*spriteWidth;
+        let positionY = index*spriteWidth;
+        frames.loc.push({x:positionX, y:positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
-    if (gameFrame % staggerFrames == 0) {
-        frameX = (frameX+1)%6;
-    }
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['idle'].loc.length;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    
     gameFrame++;
     requestAnimationFrame(animate); 
 }
