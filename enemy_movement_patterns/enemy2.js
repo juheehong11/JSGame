08f8c83801c3f77870968c642b1f87cc1aac495b
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-08-10 17:37:55 +0900

    sine cosine movement

--- enemy_movement_patterns/enemy2.js
+++ enemy_movement_patterns/enemy2.js
@@ -11,28 +11,26 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = 'enemies/enemy3.png';
+        this.image.src = 'enemies/enemy2.png';
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
         const div = Math.random()*1 + 1.5;
         this.width = this.spriteWidth / div;
         this.height = this.spriteHeight / div;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
-        this.frame = 0; 
+        this.frame = 0; // frame index
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0; 
-        this.angleSpeed = Math.random() * 2 + 0.6;
-        this.curve = Math.random() * 200 + 50;
+        this.angle = 0; // Math.random()*2;
+        this.angleSpeed = Math.random()*0.2;
+        this.curve = Math.random() * 7;
     }
     update() {
-        //this.x -= this.speed; 
-        //this.y += this.curve * Math.sin(this.angle);
-        this.x = this.curve * Math.sin(this.angle * Math.PI/90) + canvas.width/2 - this.width/2;
-        this.y = this.curve * Math.cos(this.angle * Math.PI/180) + canvas.height/2 - this.height/2;
+        this.x -= this.speed; 
         if (this.x + this.width < 0) this.x = canvas.width;
         this.angle += this.angleSpeed;
+        this.y += this.curve * Math.sin(this.angle);
         // animate sprites
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
