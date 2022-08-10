30ed2fdb749b36203b78626dff1b54af344d870b
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-08-10 19:31:58 +0900

    enemy 4 complete

--- enemy_movement_patterns/enemy3.js
+++ enemy_movement_patterns/enemy3.js
@@ -2,7 +2,7 @@
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-const numberOfEnemies = 10;
+const numberOfEnemies = 50;
 const enemiesArray = [];
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
@@ -11,31 +11,26 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = 'enemies/enemy4.png';
+        this.image.src = 'enemies/enemy3.png';
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 213;
-        this.spriteHeight = 213;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         const div = Math.random()*1 + 1.5;
         this.width = this.spriteWidth / div;
         this.height = this.spriteHeight / div;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
-        this.newX = Math.random();
-        this.newY = Math.random();
         this.frame = 0; 
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.interval = Math.floor(Math.random() * 200 + 50);
+        this.angle = Math.random() * 500; 
+        this.angleSpeed = Math.random() * 2 + 0.6;
+        this.curve = Math.random() * 200 + 50;
     }
     update() {
-        if (gameFrame % this.interval === 0) {
-            this.newX = Math.random() * (canvas.width - this.width);
-            this.newY = Math.random() * (canvas.height - this.height);
-        }
-        let dx = this.x - this.newX;
-        let dy = this.y - this.newY;
-        this.x -= dx/20;
-        this.y -= dy/20;
+        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/200) + canvas.width/2 - this.width/2;
+        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/250) + canvas.height/2 - this.height/2;
         if (this.x + this.width < 0) this.x = canvas.width;
+        this.angle += this.angleSpeed;
         // animate sprites
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
