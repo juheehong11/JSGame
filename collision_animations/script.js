32febe5a2bf990e2b95bc9490039178223fc7666
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-08-11 21:21:50 +0900

    added audio

--- collision_animations/script.js
+++ collision_animations/script.js
@@ -11,15 +11,18 @@ class Explosion {
         this.spriteHeight = 179;
         this.width = this.spriteWidth * 0.7;
         this.height = this.spriteHeight * 0.7;
-        this.x = x - this.width/2;
-        this.y = y - this.height/2;
+        this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = "boom.png";
         this.frame = 0;
         this.timer = 0;
         this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = 'sounds/ice_attack.wav'
     }
     update() {
+        if (this.frame === 0) this.sound.play();
         this.timer++;
         if (this.timer % 10 === 0) {
             this.frame++;
@@ -30,7 +33,7 @@ class Explosion {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
         ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight,
-                      this.x, this.y, this.width, this.height);
+                      0-this.width/2, 0-this.height/2, this.width, this.height);
         ctx.restore();
     }
 }
@@ -51,9 +54,9 @@ window.addEventListener('click', function(e) {
     createAnimation(e);
 });
 
-window.addEventListener('mousemove', function(e) {
-    createAnimation(e);s
-});
+// window.addEventListener('mousemove', function(e) {
+//     createAnimation(e);
+// });
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
