b93733e07d9a908d8a4e81417d9b550d44ffef76
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-08-13 22:11:27 +0900

    sound animation added

--- point_and_shoot/script.js
+++ point_and_shoot/script.js
@@ -60,6 +60,38 @@ class Raven {
     }
 }
 
+let explosions = [];
+class Explosion {
+    constructor(x, y, size) {
+        this.image = new Image();
+        this.image.src = "boom.png";
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.frame = 0;
+        this.sound = new Audio();
+        this.sound.src = "sounds/fire_impact.wav";
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 200;
+        this.markedForDeletion = false;
+    }
+    update(deltatime) {
+        if (this.frame === 0) this.sound.play();
+        this.timeSinceLastFrame += deltatime;
+        if (this.timeSinceLastFrame > this.frameInterval) {
+            this.frame++;
+            this.timeSinceLastFrame = 0;
+            if (this.frame > 5) this.markedForDeletion = true;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size/4, this.size, this.size);
+    }
+    
+}
+
 function drawScore() {
     ctx.fillStyle = "black";
     ctx.fillText("Score: " + score, 50, 75);
@@ -74,6 +106,7 @@ window.addEventListener('click', function(e) {
         if (object.randomColours[0] == pc[0]&& object.randomColours[1] == pc[1] && object.randomColours[2] == pc[2]) {
             object.markedForDeletion = true;
             score++;
+            explosions.push(new Explosion(object.x, object.y, object.width));
         }
     })
     
@@ -93,9 +126,10 @@ function animate(timestamp) {
         });
     };
     drawScore();
-    [...ravens].forEach(object => object.update(deltatime));
-    [...ravens].forEach(object => object.draw());
+    [...ravens, ...explosions].forEach(object => object.update(deltatime));
+    [...ravens, ...explosions].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
+    explosions = explosions.filter(object => !object.markedForDeletion);
     requestAnimationFrame(animate);
 }
 animate(0);
