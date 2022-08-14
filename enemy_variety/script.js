0b659eaf70034a7ee3936941ab22142b731f93ae
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-08-14 19:54:32 +0900

    delete squares out of screen

--- enemy_variety/script.js
+++ enemy_variety/script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
             this.enemyTimer = 0;
         }
         update(deltaTime) {
+            this.enemies = this.enemies.filter(object => !object.markedForDeletion);
             if (this.enemyTimer > this.enemyInterval) {
                 this.#addNewEnemy();
                 this.enemyTimer = 0;
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
             this.enemies.forEach(object => object.update());
         }
         draw() {
-            this.enemies.forEach(object => object.draw());
+            this.enemies.forEach(object => object.draw(this.ctx));
         }
         #addNewEnemy() {
             this.enemies.push(new Enemy(this));
@@ -37,12 +38,13 @@ document.addEventListener('DOMContentLoaded', function() {
             this.y = Math.random() * this.game.height;
             this.width = 100;
             this.height = 100;
-
+            this.markedForDeletion = false;
         }
         update() {
             this.x--;
+            if (this.x < 0 - this.width) this.markedForDeletion = true;
         }
-        draw() {
+        draw(ctx) {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
