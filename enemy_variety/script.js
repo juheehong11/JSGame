f900b9d3355c334a87219741a05decd97811bafd
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-08-14 21:05:38 +0900

    ghost class complete

--- enemy_variety/script.js
+++ enemy_variety/script.js
@@ -31,9 +31,9 @@ document.addEventListener('DOMContentLoaded', function() {
             const randomEnemy = this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)];
             if (randomEnemy === "worm") this.enemies.push(new Worm(this));
             else if (randomEnemy === "ghost") this.enemies.push(new Ghost(this));
-            this.enemies.sort(function(a, b) {
-                return a.y - b.y;
-            });
+            // this.enemies.sort(function(a, b) {
+            //     return a.y - b.y;
+            // });
         }
     }
 
@@ -56,10 +56,10 @@ document.addEventListener('DOMContentLoaded', function() {
             super(game);
             this.spriteWidth = 229;
             this.spriteHeight = 171;
-            this.x = this.game.width;
-            this.y = Math.random() * this.game.height;
             this.width = this.spriteWidth/2;
             this.height = this.spriteHeight/2;
+            this.x = this.game.width;
+            this.y = this.game.height - this.height;
             this.image = worm;
             this.vx = Math.random() * 0.1 + 0.1;
         }
@@ -71,11 +71,24 @@ document.addEventListener('DOMContentLoaded', function() {
             this.spriteWidth = 261;
             this.spriteHeight = 209;
             this.x = this.game.width;
-            this.y = Math.random() * this.game.height;
+            this.y = Math.random() * this.game.height * 0.6;
             this.width = this.spriteWidth/2;
             this.height = this.spriteHeight/2;
             this.image = ghost;
             this.vx = Math.random() * 0.2 + 0.1;
+            this.angle = 0;
+            this.curve = Math.random() * 3;
+        }
+        update(deltaTime) {
+            super.update(deltaTime);
+            this.y += Math.sin(this.angle) * this.curve;
+            this.angle += 0.04;
+        }
+        draw() {
+            ctx.save();
+            ctx.globalAlpha = 0.5;
+            super.draw(ctx);
+            ctx.restore();
         }
     }
 
